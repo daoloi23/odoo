4e6d0c5c0b6ea4aba6494fd744dcff3504f3ca55
daoloi23 <daoducloi23@gmail.com>
2025-11-03 11:46:44 +0700

    create module

---
+++ daotaoierp/training/ierptraining/__manifest__.py
@@ -0,0 +1,16 @@
+# __manifest__.py
+{
+    'name': 'ierp training',
+    'version': '19.0.1.0.1',
+    'summary': 'Simple odoo training',
+    'author': 'loidd',
+    'license' : 'OEEL-1',
+    'category': 'Productivity',
+    'depends': ['crm'],
+    'data': [
+
+
+    ],
+    'installable': True,
+    'application': True,
+}
